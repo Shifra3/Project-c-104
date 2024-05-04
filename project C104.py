919e1c58e8987e3af232d98295c61c28c5d6837a
Shifra3 <72134790+Shifra3@users.noreply.github.com>
2024-05-04 13:09:21 +0530

    Add files via upload

---
+++ project C104.py
@@ -0,0 +1,13 @@
+import cv2
+img=cv2.imread("solar-system.jpg")
+cv2.putText(img, "Sun", (20,300),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255))
+cv2.putText(img, "Mercury", (120,200),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img, "Venus", (170,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img, "Earth", (290,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img, "Mars", (370,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img, "Jupiter", (450,350),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img, "Saturn", (840,150),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img, "Uranus", (920,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img, "Neptune", (1100,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.imshow("image",img)
+cv2.waitKey(0)
